fc2ac902832f96f0b5b30a38043064a39ee89318
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2020-12-06 22:45:46 -0500

    wsd: log: overload chrono duration to simplify logging
    Also, makes the logging of units much less error prone.
    
    The overloaded streaming operators are temporary as
    they are provided in C++20. The ones here (though
    incomplete) are fashioned after the C++20 specs.
    
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>
    (cherry picked from commit ba4e52e7b9722f18379d30954a94e95d00c84d3e)
    
    Change-Id: Ieb499282ccb6e63fa939ba07bed3e5a4fbef1bd0

--- wsd/Storage.cpp
+++ wsd/Storage.cpp
@@ -658,9 +658,9 @@ std::unique_ptr<WopiStorage::WOPIFileInfo> WopiStorage::getWOPIFileInfo(const Au
     if (JsonUtil::parseJSON(wopiResponse, object))
     {
         if (LOOLWSD::AnonymizeUserData)
-            LOG_DBG("WOPI::CheckFileInfo (" << callDurationMs.count() << " ms): anonymizing...");
+            LOG_DBG("WOPI::CheckFileInfo (" << callDurationMs << "): anonymizing...");
         else
-            LOG_DBG("WOPI::CheckFileInfo (" << callDurationMs.count() << " ms): " << wopiResponse);
+            LOG_DBG("WOPI::CheckFileInfo (" << callDurationMs << "): " << wopiResponse);
 
         std::size_t size = 0;
         std::string filename, ownerId, lastModifiedTime;
@@ -692,9 +692,8 @@ std::unique_ptr<WopiStorage::WOPIFileInfo> WopiStorage::getWOPIFileInfo(const Au
             wopiResponse = "obfuscated";
 
         LOG_ERR("WOPI::CheckFileInfo ("
-                << callDurationMs.count()
-                << " ms) failed or no valid JSON payload returned. Access denied. "
-                   "Original response: ["
+                << callDurationMs
+                << ") failed or no valid JSON payload returned. Access denied. Original response: ["
                 << wopiResponse << "].");
 
         throw UnauthorizedRequestException("Access denied. WOPI::CheckFileInfo failed on: " + uriAnonym);
@@ -785,7 +784,7 @@ WopiStorage::WOPIFileInfo::WOPIFileInfo(const FileInfo &fileInfo,
     else
         object->stringify(wopiResponse);
 
-    LOG_DBG("WOPI::CheckFileInfo (" << callDurationMs.count() << " ms): " << wopiResponse.str());
+    LOG_DBG("WOPI::CheckFileInfo (" << callDurationMs << "): " << wopiResponse.str());
 
     JsonUtil::findJSONValue(object, "UserExtraInfo", _userExtraInfo);
     JsonUtil::findJSONValue(object, "WatermarkText", _watermarkText);
