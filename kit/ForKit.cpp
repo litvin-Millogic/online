216bb51d71e7966e2a9e0ed2586d5f6ab8fc09ed
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2021-03-27 09:39:03 -0400

    wsd: pass the config from loolwsd to forkit
    Change-Id: If89a5021bc7d86d630511abb65839f5448f4bd3d
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>
    (cherry picked from commit db36d079cee2b788cd429137c921c6fae1e29877)

--- kit/ForKit.cpp
+++ kit/ForKit.cpp
@@ -335,6 +335,7 @@ static int createLibreOfficeKit(const std::string& childRoot,
                                 const std::string& sysTemplate,
                                 const std::string& loTemplate,
                                 const std::string& loSubPath,
+                                const Poco::Util::XMLConfiguration& xmlConfig,
                                 bool queryVersion = false)
 {
     // Generate a jail ID to be used for in the jail path.
@@ -374,7 +375,7 @@ static int createLibreOfficeKit(const std::string& childRoot,
         }
 
         lokit_main(childRoot, jailId, sysTemplate, loTemplate, loSubPath, NoCapsForKit, NoSeccomp,
-                   queryVersion, DisplayVersion, spareKitId);
+                   queryVersion, DisplayVersion, spareKitId, xmlConfig);
     }
     else
     {
@@ -401,6 +402,7 @@ void forkLibreOfficeKit(const std::string& childRoot,
                         const std::string& sysTemplate,
                         const std::string& loTemplate,
                         const std::string& loSubPath,
+                        const Poco::Util::XMLConfiguration& xmlConfig,
                         int limit)
 {
     // Cleanup first, to reduce disk load.
@@ -421,7 +423,9 @@ void forkLibreOfficeKit(const std::string& childRoot,
         const size_t retry = count * 2;
         for (size_t i = 0; ForkCounter > 0 && i < retry; ++i)
         {
-            if (ForkCounter-- <= 0 || createLibreOfficeKit(childRoot, sysTemplate, loTemplate, loSubPath) < 0)
+            if (ForkCounter-- <= 0
+                || createLibreOfficeKit(childRoot, sysTemplate, loTemplate, loSubPath, xmlConfig)
+                       < 0)
             {
                 LOG_ERR("Failed to create a kit process.");
                 ++ForkCounter;
@@ -610,6 +614,7 @@ int main(int argc, char** argv)
             SingleKit = true;
         }
 #endif
+
         // we are running in a lower-privilege mode - with no chroot
         else if (std::strstr(cmd, "--nocaps") == cmd)
         {
@@ -675,12 +680,18 @@ int main(int argc, char** argv)
     // Make dev/[u]random point to the writable devices in tmp/dev/.
     JailUtil::SysTemplate::setupRandomDeviceLinks(sysTemplate);
 
+    // Parse the configuration.
+    const auto conf = std::getenv("LOOL_CONFIG");
+    std::istringstream iss(std::string(conf ? conf : std::string()));
+    Poco::AutoPtr<Poco::Util::XMLConfiguration> xmlConfig(new Poco::Util::XMLConfiguration(iss));
+
     LOG_INF("Preinit stage OK.");
 
     // We must have at least one child, more are created dynamically.
     // Ask this first child to send version information to master process and trace startup.
     ::setenv("LOOL_TRACE_STARTUP", "1", 1);
-    pid_t forKitPid = createLibreOfficeKit(childRoot, sysTemplate, loTemplate, loSubPath, true);
+    const pid_t forKitPid
+        = createLibreOfficeKit(childRoot, sysTemplate, loTemplate, loSubPath, *xmlConfig, true);
     if (forKitPid < 0)
     {
         LOG_FTL("Failed to create a kit process.");
@@ -720,7 +731,7 @@ int main(int argc, char** argv)
 #if ENABLE_DEBUG
         if (!SingleKit)
 #endif
-        forkLibreOfficeKit(childRoot, sysTemplate, loTemplate, loSubPath);
+            forkLibreOfficeKit(childRoot, sysTemplate, loTemplate, loSubPath, *xmlConfig);
     }
 
     int returnValue = EX_OK;
